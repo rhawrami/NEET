f8ba3addcd039b4eb571eedd8892c43f3b7d555f
r h <ravanhawrami@gmail.com>
2025-05-14 10:51:13 -0500

    final analysis notebook

--- scripts/clean_ipums.py
+++ scripts/clean_ipums.py
@@ -32,9 +32,7 @@ def get_CPS(years, vars = ['AGE'], filename='', filepath=''):
     string of directory where files should go
 
     &nbsp;ex. filename = 'datasets'
-
     """
-    
     
     sampleID_CPS = 'https://cps.ipums.org/cps-action/samples/sample_ids' # url to CPS sample id's
     content_CPS = requests.get(sampleID_CPS).text
@@ -86,16 +84,78 @@ def get_CPS(years, vars = ['AGE'], filename='', filepath=''):
     default_data_files.rename(renamed_data_files)
     default_ddi_files.rename(renamed_ddi_files)
 
+def get_ACS(five_year = True, years = 2023, vars = ['age'], filename = '', filepath = ''):
+    """collects IPUMS CPS data and downloads extract
+    
+    ## Args:
+    
+    #### years:
+    a tuple of years, or single integer year to include
 
+    &nbsp;ex. years = (2000,2023)
+
+    #### vars:
+    list of variables to include
+
+    &nbsp;ex. vars = ['AGE', 'SEX', 'EMPSTAT']
+
+    #### filename:
+    string of desired file name 
+
+    &nbsp;ex. filename = 'cps_16to24_2023'
+
+    #### filepath:
+    string of directory where files should go
+
+    &nbsp;ex. filename = 'datasets'
+    """
+    IPUMS_API_KEY = os.environ.get("IPUMS_API_KEY") # get API environmental variable
+    
+    client_API = IpumsApiClient(IPUMS_API_KEY)
+
+    if isinstance(years, int): # if only one year is supplied
+        start = end = years
+    else:
+        start, end = years # get start and end range for years to include
+
+    if five_year:
+        cleaned_samples_list = [f'us{yr}c' for yr in range(start, end + 1)]
+    elif not five_year:
+        cleaned_samples_list = [f'us{yr}a' for yr in range(start, end + 1)]
+
+    extract = MicrodataExtract(
+        collection='usa',
+        samples=cleaned_samples_list,
+        description='ACS Extract for NEETs',
+        variables=vars
+    )
+    
+    # submit, wait, download extract
+    client_API.submit_extract(extract=extract) 
+
+    client_API.wait_for_extract(extract)
+
+    client_API.download_extract(extract, download_dir=filepath + '/') # download extract 
+
+    # rename files from cps_0000[X].[dat.gz/xml] to our filename parameter
+    default_data_files = Path(glob.glob(filepath + '/' 'usa_0*.dat.gz')[0])
+    default_ddi_files = Path(glob.glob(filepath + '/' 'usa_0*.xml')[0])
+
+    renamed_data_files = filepath + '/' + filename + '.dat.gz'
+    renamed_ddi_files = filepath + '/' + filename + '.xml'
+
+    default_data_files.rename(renamed_data_files)
+    default_ddi_files.rename(renamed_ddi_files)
 
 
 if __name__ == '__main__':
-    get_CPS(years=(2022,2023), vars=['AGE', 'SEX'], filename='test_cps', filepath='datasets/') # test if I can pull data from IPUMS API, and rename file
+    # get_CPS(years=(2022,2023), vars=['AGE', 'SEX'], filename='test_cps', filepath='datasets') # test if I can pull data from IPUMS API, and rename file
 
-    ddi = readers.read_ipums_ddi('datasets/test_cps.xml')
-    dff = readers.read_microdata(ddi=ddi, filename='datasets/test_cps.dat.gz')
+    # ddi = readers.read_ipums_ddi('datasets/test_cps.xml')
+    # dff = readers.read_microdata(ddi=ddi, filename='datasets/test_cps.dat.gz')
 
-    print(dff.head())
-    print('mean age in data:', np.mean(dff['AGE']))
-    print('share of men in sample:', (dff['SEX'] == 1).mean())
+    # print(dff.head())
+    # print('mean age in data:', np.mean(dff['AGE']))
+    # print('share of men in sample:', (dff['SEX'] == 1).mean())
     
+    get_ACS(five_year=True,years=(2022,2023), vars=['AGE', 'SEX'], filename='test_acs', filepath='datasets')
